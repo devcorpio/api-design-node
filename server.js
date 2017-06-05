4101f695ac7c34b60a4756182899fe8ac053d9f1
Alberto Delgado <albert.delgado.2016@salleurl.edu>
2017-06-05 19:43:40 +0200

    show index.html content when server it is visited through / path

--- server.js
+++ server.js
@@ -2,8 +2,16 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 var express = require('express');
+var fs = require('fs');
 var jsonData = {count: 12, message: 'hey'};
 
 var app = express();
+
+app.get('/', function (req, res) {
+  fs.readFile('./index.html', function (err, html) {
+    res.write(html);
+    res.end();
+  });
+})
 
 app.listen(3000);
