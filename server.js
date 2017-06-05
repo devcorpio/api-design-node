a37de4551882fb0c24975d57da4d3263e79d9260
Alberto Delgado <albert.delgado.2016@salleurl.edu>
2017-06-05 19:37:37 +0200

    create basic server

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
-
+var express = require('express');
 var jsonData = {count: 12, message: 'hey'};
+
+var app = express();
+
+app.listen(3000);
